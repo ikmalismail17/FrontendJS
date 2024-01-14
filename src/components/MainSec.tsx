55f09a18827cb8785875b4019ab7786f310d01a0
ikmalismail17 <ikmal5a2013@gmail.com>
2024-01-15 01:44:42 +0800

    Update MainSec.tsx

--- src/components/MainSec.tsx
+++ src/components/MainSec.tsx
@@ -94,7 +94,7 @@ function a11yProps(index: number) {
 function MainSec() {
   const [data, setData] = useState<DataItem[]>([]);
   const [value, setValue] = React.useState(1);
-  const [isHighDepth] = React.useState(false);
+  const [isHighDepth, setIsHighDepth] = React.useState(false);
 
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -135,16 +135,16 @@ useEffect(() => {
   }, []);
 
   const dateUI = new Date();
-  console.log(dayjs(dateUI).format('MM/DD/YYYY'));
+  console.log(dayjs(dateUI).format('DD/MM/YYYY'));
 
-  // const hasHighDepth = data
-  // .slice()
-  // .reverse()
-  // .some((item) => {
-  //   return dayjs(item.date, 'DD/MM/YYYY').isSame(dayjs(dateUI).format('MM/DD/YYYY'), 'day') && item.distanceCm > 200;
-  // });
+  const hasHighDepth = data
+  .slice()
+  .reverse()
+  .some((item) => {
+    return dayjs(item.date, 'DD/MM/YYYY').isSame(dayjs(dateUI).format('MM/DD/YYYY'), 'day') && item.distanceCm > 200;
+  });
 
-  // setIsHighDepth(hasHighDepth);
+  setIsHighDepth(hasHighDepth);
 
   return (
     <React.Fragment>
